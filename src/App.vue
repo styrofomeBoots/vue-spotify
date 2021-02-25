41af0c3f01d320be8d39d3dc4da55925acf6ff90
Seth <seth.newbill@temperpack.com>
2021-02-25 17:07:50 -0500

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-dark h-screen">
+    <div class="flex" style="height: 88vh">
+      <!-- side nav -->
+      <div class="w-56 bg-black h-full flex-none">
+        <div class="p-6">
+          <img src="./assets/img/Spotify_Logo_RGB_White.png" class="h-10" />
+        </div>
+        <div class="mx-2 mb-5">
+          <button
+            v-for="page in pages"
+            @click="setID = page.id"
+            :key="page.id"
+            :class="`w-full text-sm font-semibold rounded px-3 py-2 flex items-center justify-start  ${
+              setID === page.id ? 'bg-light text-white' : 'text-lightest'
+            }`"
+          >
+            <i class="material-icons mr-3">{{ page.icon }}</i>
+            <p>{{ page.name }}</p>
+          </button>
+        </div>
+        <div class="mx-5">
+          <h1 class="mb-3 text-xs text-lightest tracking-widest uppercase">
+            Playlists
+          </h1>
+          <button class="flex items-center justify-start">
+            <img src="./assets/img/add-new.png" class="h-8 w-8 mr-3" />
+            <p class="text-sm text-white font-semibold">Create Playlist</p>
+          </button>
+        </div>
+      </div>
+      <!-- main content -->
+      <div class="w-full h-full relative">
+        <!-- header -->
+        <div class="w-full sticky top-0 p-2"></div>
+      </div>
+    </div>
+    <!-- play bar -->
+    <div class="w-full bg-light" style="height: 12vh"></div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: function () {
+    return {
+      pages: [
+        { id: "home", name: "Home", icon: "home" },
+        { id: "search", name: "Search", icon: "search" },
+        { id: "library", name: "Your Library", icon: "bar_chart" },
+      ],
+      setID: "home",
+    };
+  },
+  methods: {},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+button:focus {
+  outline: 0;
 }
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: 'Proxima Nova',
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        'green': '#1D8954',
+        'dark': '#121212',
+        'light': '#282828',
+        'lightest': '#838383',
+        'darkest': '#191414'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
